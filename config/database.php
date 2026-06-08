40e3368bbe37012e7260d483a9542ec7120c11f6
AbHaMaDa <abdallahhamada2103@gmail.com>
2026-06-08 04:25:44 +0300

    Allow disabling MySQL SSL cert verify via DB_SSL_VERIFY env

--- config/database.php
+++ config/database.php
@@ -60,7 +60,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY', false),
+            ], fn ($v) => $v !== null) : [],
         ],
 
         'mariadb' => [
@@ -80,7 +81,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY', false),
+            ], fn ($v) => $v !== null) : [],
         ],
 
         'pgsql' => [
