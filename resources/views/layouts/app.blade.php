4c6d4dc676ac2746fa4da852bec2b4789f610fc5
AbHaMaDa <abdallahhamada2103@gmail.com>
2026-06-08 04:51:01 +0300

    Cache-bust script.js by filemtime to prevent stale browser cache

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/js/script.js"></script>
+    <script src="/assets/js/script.js?v={{ filemtime(public_path('assets/js/script.js')) }}"></script>
 
 
 
